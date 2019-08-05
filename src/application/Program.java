22575895e461983e8879ee32cd0f612b158420d0
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2019-08-05 18:14:41 -0300

    Criando Classe Program e importando Rent

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rent[] vect = new Rent[10];
+
+	}
+
+}
